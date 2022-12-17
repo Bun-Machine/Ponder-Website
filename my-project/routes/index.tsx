04f6a699c384664a8492c1df37195ebd8e3eb793
Sam Goldenberg <sgolden140@gmail.com>
2022-12-17 11:16:19 -0800

    figuring it out

--- my-project/routes/index.tsx
+++ my-project/routes/index.tsx
@@ -9,9 +9,9 @@ export default function Home() {
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
         <img
-          src="../static/deno-logo-beta2.png"
+          src="/ponder1.png"
           class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
+          alt="the Ponder logo: the Deno dinosaur pondering in front of two databases"
         />
         <p class="my-6">
           Come wander to Ponder!

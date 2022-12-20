254f7a0f5957f5be8a7ba1b5e7ebabd770f4ca77
Sam Goldenberg <sgolden140@gmail.com>
2022-12-19 16:54:07 -0800

    added some components to make copying easier, changing colors and logos to match

--- my-project/routes/index.tsx
+++ my-project/routes/index.tsx
@@ -19,6 +19,9 @@ import IconAirBalloon from "https://deno.land/x/tabler_icons_tsx@0.0.2/tsx/air-b
 import IconArmchair from "https://deno.land/x/tabler_icons_tsx@0.0.2/tsx/armchair.tsx";
 import IconChevronRight from "https://deno.land/x/tabler_icons_tsx@0.0.2/tsx/chevron-right.tsx";
 
+import CopyArea from "../islands/CopyArea.tsx";
+import * as Icons from "../components/Icons.tsx";
+
 export default function Home() {
   return (
     <div class="bg-pink-400 flex-col items-center">
@@ -36,7 +39,7 @@ export default function Home() {
         /* <GettingStarted />
       <Examples /> */
       }
-
+      <GettingStarted />
       <Footer />
       {/* <SamTest /> */}
     </div>
@@ -65,7 +68,7 @@ function Header({ active }: Props) {
   ];
 
   return (
-    <div class="flex justify-center bg-white w-full py-6 px-8 flex flex-col md:flex-row gap-4 justify-center">
+    <div style="background:#e9cceb; color: #1C1922" class="flex justify-center w-full py-6 px-8 flex flex-col md:flex-row gap-4 justify-center">
       <div class="flex items-center flex-1">
         <IconBrandDeno class="w-6 h-6" />
         <div class="text-2xl  ml-1 font-bold">
@@ -77,7 +80,7 @@ function Header({ active }: Props) {
           <li>
             <a
               href={menu.href}
-              class={"text-gray-500 hover:text-gray-700 py-1 border-gray-500" +
+              class={"hover:text-gray-700 py-1 border-gray-500" +
                 (menu.href === active ? " font-bold border-b-2" : "")}
             >
               {menu.name}
@@ -91,9 +94,9 @@ function Header({ active }: Props) {
 
 function NavBar() {
   return (
-    <div class="bg-green-400 justify-center flex">
+    <div class="bg-purple-400 justify-center flex border(b purple-500)">
       <a
-        class="bg-green-400 text-black border(b green-500) p-3 text-center group"
+        class="bg-purple-400 text-black p-3 text-center group"
         href="https://deno.land/x/ponder/"
       >
         <b>Ponder v0.0.3.1</b> has been released, still in pre-production!{" "}
@@ -106,16 +109,20 @@ function NavBar() {
 
 function WelcomeArea() {
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
+    <div class="flex flex-col items-center justify-center p-4 mx-auto ">
+      <h2 class="py-2 text(5xl sm:5xl lg:5xl gray-900) sm:tracking-tight sm:leading-[1.1]! font-extrabold">
+            A <span class="text-purple-600">simple ORM</span> for PostGres, built for Deno.
+          </h2>
       <img
         src="/ponder1.png"
-        class="w-32 h-32"
+        class="w-120 h-120"
         alt="the Ponder logo: the Deno dinosaur pondering in front of two databases"
       />
-      <p class="my-6">
+      <p class="my-6 text-4xl font-extrabold font-sans" >
         Come wander to Ponder!
       </p>
-      <Counter start={3} />
+      {/* <Counter start={3} /> */}
+      
     </div>
   );
 }
@@ -138,7 +145,7 @@ function Hero() {
       style="background-image:linear-gradient(rgba(0, 0, 40, 0.8),rgba(0, 0, 40, 0.8)), url('/gallery/hero-bg.webp');"
     >
       <div class="space-y-4 text-center">
-        <h1 class="text-4xl inline-block font-bold">Fresh Components Beta</h1>
+        <h1 class="text-4xl inline-block font-bold">Object Relational Mapping Tool</h1>
         <p class="text-xl max-w-lg text-blue-100">
           Fresh Components is a collection of components built with Preact and
           Tailwind CSS.
@@ -212,15 +219,88 @@ function Features() {
   );
 }
 
-// function GettingStarted() {
-//   return(
-//     <div>
-//       <div>
-//         Getting Started Area
-//       </div>
-//     </div>
-//   )
-// }
+function GettingStarted(props: { origin: string }) {
+  return (
+    <section class="max-w-screen-md mx-auto my-16 px(4 sm:6 md:8) space-y-4">
+      <h2 id="getting-started" class="text(3xl gray-600) font-bold">
+        <a href="#getting-started" class="hover:underline">
+          Getting Started
+        </a>
+      </h2>
+      <div class="text-gray-600 flex gap-1 mb-4 bg-gray-100 p-2 rounded">
+        <div class="text-gray-400">
+          <Icons.Info />
+        </div>
+        <p>
+          <a href="https://deno.land" class="text-blue-600 hover:underline">
+            Deno CLI
+          </a>{" "}
+          version 1.25.0 or higher is required.{" "}
+          <a
+            href="https://deno.land/manual/getting_started/installation"
+            class="text-blue-600 hover:underline"
+          >
+            Install
+          </a>{" "}
+          or{" "}
+          <a
+            href="https://deno.land/manual/getting_started/installation#updating"
+            class="text-blue-600 hover:underline"
+          >
+            update
+          </a>.
+        </p>
+      </div>
+      <p class="text-gray-600">
+        To include Ponder in your next project, add this dependency:
+      </p>
+
+      <CopyArea>
+        {`import * as ponder from "https://deno.land/x/ponder@v0.0.3.1/mod.ts";`}
+      </CopyArea>
+
+      <p class="text-gray-600">
+        You'll now be able to connect Ponder to your Database. First, store your Database URI in your own .env file. Then you can pull into your working file, the Database URI.
+      </p>
+
+      <CopyArea>{`const DB_URI = Deno.env.get('DB_URI');`}</CopyArea>
+
+      <p class="text-gray-600">
+        Now you can connect to your DB using the built-in poolConnection method: 
+      </p>
+
+      <CopyArea>{'const ponderDB1 = await ponder.poolConnection(`${DB_URI}`, 3, true);'}</CopyArea>
+
+      <p class="text-gray-600">
+        Now you are now able to run any of the built in functions off the ponderDB1 variable: 
+      </p>
+
+      <CopyArea>{"const inserted = await ponderDB1.insertIntoTable('people', ['name', 'hair_color'], ['Corey', 'red-brown']);"}</CopyArea>
+
+      {/* <p class="text-gray-600">
+        You can now open{" "}
+        <a
+          href="http://localhost:8000"
+          class="text-blue-600 hover:underline"
+        >
+          http://localhost:8000
+        </a>{" "}
+        in your browser to view the page.
+  </p>}}*/}
+      <p class="text-gray-600"> 
+        A more in-depth{" "}
+        <a
+          href="/docs/getting-started"
+          class="text-blue-600 hover:underline"
+        >
+          <i>Getting Started</i>
+        </a>{" "}
+        guide is available in{" "}
+        <a href="/docs" class="text-blue-600 hover:underline">the docs</a>.
+      </p>
+    </section>
+  );
+}
 
 // function Examples() {
 //   return(
@@ -268,11 +348,11 @@ function Footer({ children }: Props) {
       <div class="flex-1">
         <div class="flex items-center gap-1">
           <div class="font-bold text-2xl">
-            Fresh
+            Ponder
           </div>
         </div>
         <div class="text-gray-500">
-          Full Stack Framework
+          Simple ORM for PostGresSQL in Deno.
         </div>
       </div>
 

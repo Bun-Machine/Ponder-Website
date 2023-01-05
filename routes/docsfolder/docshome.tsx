65e3238d4202056b7fdc7bdd6088e7ff1e56095b
Sam Goldenberg <sgolden140@gmail.com>
2023-01-04 20:07:42 -0800

    completed about sections with social links and photos

--- routes/docsfolder/docshome.tsx
+++ routes/docsfolder/docshome.tsx
@@ -32,7 +32,7 @@ export default function DocsHome() {
       <DatabaseIntrospection />
       <AssociationsDoc />
       <CLI />
-      <ExampleDoc />
+      <ContributionAndBugs />
       <AboutDoc />
       <Footer />
     </div>
@@ -70,9 +70,9 @@ export function DocsTableOfContents() {
     { name: "Basic CRUD", href: "#basicCRUD" },
     { name: "Managing Tables", href: "#managingTables" },
     { name: "Database Introspection", href: "#databaseIntrospection" },
-    { name: "AssociationsDoc", href: "#associationsDoc" },
+    { name: "Associations", href: "#associationsDoc" },
     { name: "CLI", href: "#CLI" },
-    { name: "Examples", href: "#examplesDoc" },
+    { name: "Making Contribution, Finding Bugs", href: "#contributeBugs" },
     { name: "About", href: "#aboutDoc" },
   ];
   const menus = [
@@ -401,7 +401,7 @@ export function AssociationsDoc() {
     <section id="associationsDoc" class="max-w-screen-md mx-auto my-16 px(4 sm:6 md:8) space-y-4">
     <h2 class="text(3xl gray-600) font-bold">Associations</h2>
       <p>
-        Coming Soon!
+        Currently under development!
       </p>
     </section>
   );
@@ -413,26 +413,31 @@ export function CLI() {
     <section id="CLI" class="max-w-screen-md mx-auto my-16 px(4 sm:6 md:8) space-y-4">
     <h2 class="text(3xl gray-600) font-bold">Ponder Command Line Interface (CLI)</h2>
       <p>
-        Coming Soon!
+        Coming Soon! A feature for the future!
       </p>
     </section>
   );
 }
 
-export function ExampleDoc() {
+export function ContributionAndBugs() {
   return (
-    <section id="examplesDoc" class="max-w-screen-md mx-auto my-16 px(4 sm:6 md:8) space-y-4">
-    <h2 class="text(3xl gray-600) font-bold">Examples of Ponder in Use</h2>
+    <section id="contributeBugs" class="max-w-screen-md mx-auto my-16 px(4 sm:6 md:8) space-y-4">
+    <h2 class="text(3xl gray-600) font-bold">Making Contributions & Finding Bugs</h2>
 
       <p>
-        Here are some examples of ponder in the wild:
-        <br></br>
-        <br></br>
-        (...crickets...)
+      To make contributions, create a fork of the dev branch. Please be sure to utilize the Deno Typescript linter. Ensure that any changes made are reflected in the documentation. Make a pull request to the Dev branch when you have finished making your changes, note that once submitted any changes made will be covered under the MIT license.
+      Feel free to contact the maintainers with any questions or concerns.
+      <h3 class="text(xl gray-600) font-bold">Finding Bugs</h3>
+      If you come across any bugs or issues while using Ponder feel free to report by simply opening a new issue on our Github.
+
+      <h3 class="text(xl gray-600) font-bold">License</h3>
+      MIT License
+
+      
       </p>
     </section>
   );
-}
+};
 
 export function AboutDoc() {
     return (
@@ -443,36 +448,69 @@ export function AboutDoc() {
 
           <div id='bioBoxMatt' class='flex flex-row m-1'>
             <img src="/mattC (1).png" class="rounded-full w-48 h-48"></img>
-            <p>
-              <h3><b>Matt Connell</b></h3>
-              <a href="https://github.com/Matt-2112">GitHub</a> 
-              <br></br>
-              <a href="https://www.linkedin.com/in/matt-connell-/">LinkedIn</a>
-            </p>
+            <div class="ml-2 mt-auto mb-auto">
+              <p>
+               <h3><b>Matt Connell</b></h3>
+               <h4><b>Software Engineer</b></h4>
+               <a class ="flex flex-row justify-start gap-1" href="https://github.com/Matt-2112" target="_blank">GitHub <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></span></a> 
+                <br></br>
+                <a class ="flex flex-row justify-start gap-1 -mt-4" href="https://www.linkedin.com/in/matt-connell-/" target="_blank">LinkedIn <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
+             </p>
+            </div>
           </div>
 
           <div id='bioBoxSam' class='flex flex-row flex-col m-1'>
-            <img src="/SammyG.png" class="rounded-full w-48 h-48"></img>
-            <a href="https://github.com/sammyb1rd" target="_blank">GitHub</a> 
-            <a href="https://www.linkedin.com/in/samuel-goldenberg/" target="_blank">LinkedIn</a>
+               <img src="/SammyG.png" class="rounded-full w-48 h-48"></img>
+               <div class="ml-2 mt-auto mb-auto">
+                <p>
+                <h3><b>Sam Goldenberg</b></h3>
+                <h4><b>Software Engineer</b></h4>
+                <a class ="flex flex-row justify-start gap-1" href="https://github.com/sammyb1rd" target="_blank">GitHub <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></span></a> 
+                <br></br>
+                <a class ="flex flex-row justify-start gap-1 -mt-4" href="https://www.linkedin.com/in/samuel-goldenberg/" target="_blank">LinkedIn <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
+               </p>
+            </div>
           </div>
 
           <div id='bioBoxStella' class='flex flex-row flex-col m-1'>
             <img src="/Stella (1).png" class="rounded-full w-48 h-48"></img>
-            <a href="https://github.com/StellaBaek" target="_blank">GitHub</a> 
-            <a href="https://www.linkedin.com/in/stellabaek/" target="_blank">LinkedIn</a>
+            <div class="ml-2 mt-auto mb-auto">
+              <p>
+               <h3><b>Stella Baek</b></h3>
+               <h4><b>Software Engineer</b></h4>
+            <a class ="flex flex-row justify-start gap-1 " href="https://github.com/StellaBaek" target="_blank">GitHub <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></span></a> 
+            <br></br>
+            <a class ="flex flex-row justify-start gap-1 -mt-4" href="https://www.linkedin.com/in/stellabaek/" target="_blank">LinkedIn <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
+              </p>
+            </div>
           </div>
 
           <div id='bioBoxCorey' class='flex flex-row flex-col m-1'>
             <img src="/Corey.png" class="rounded-full w-48 h-48"></img>
-            <a href="https://github.com/mcbrownc" target="_blank">GitHub</a> 
-            <a href="https://www.linkedin.com/in/coreymcclendonbrown/" target="_blank">LinkedIn</a>
+            <div class="ml-2 mt-auto mb-auto">
+              <p>
+               <h3><b>Corey McClendon-Brown</b></h3>
+               <h4><b>Software Engineer</b></h4>
+            <a class ="flex flex-row justify-start gap-1 " href="https://github.com/mcbrownc" target="_blank">GitHub <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></span></a> 
+            <br></br>
+            <a class ="flex flex-row justify-start gap-1 -mt-4" href="https://www.linkedin.com/in/coreymcclendonbrown/" target="_blank">LinkedIn <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
+             </p>
+            </div>
           </div>
 
           <div id='bioBoxSara' class='flex flex-row flex-col m-1'>
             <img src="/SaraB.png" class="rounded-full w-48 h-48"></img>
-            <a href="https://github.com/Sbrown2018" target="_blank">GitHub</a> 
-            <a href="https://www.linkedin.com/in/sara-brown15/" target="_blank">LinkedIn</a>
+            <div class="ml-2 mt-auto mb-auto">
+              {/* <p> */}
+               <h3><b>Sara Brown</b></h3>
+               <h4><b>Software Engineer</b></h4>
+              <div class ="flex flex-row justify-start">
+            <a class ="flex flex-row justify-start gap-1 " href="https://github.com/Sbrown2018" target="_blank">GitHub <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></span></a> 
+            </div>
+            <br></br>
+            <a class ="flex flex-row justify-start gap-1 -mt-4" href="https://www.linkedin.com/in/sara-brown15/" target="_blank">LinkedIn <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
+             {/* </p> */}
+            </div>
           </div>
 
         </div>
